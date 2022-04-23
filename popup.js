d1fff4c1424269093b6e0ae1ee0db3816e14bb67
Joe Murphy <murphcle@gmail.com>
2022-04-23 05:45:11 -0400

    the pieces work

--- popup.js
+++ popup.js
@@ -1,10 +1,6 @@
-// Initialize butotn with users's prefered color
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+let wordsLeftText = document.getElementById("wordsLeft")
 
 async function oneCall(bodyData) {
-  console.log(JSON.stringify(bodyData))
   const response = await fetch(`https://1vv6d7.deta.dev/onecall`, {
     method: 'POST',
     headers: {
@@ -35,27 +31,16 @@ runContentScript()
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
+    console.log(`received msg from content script: ${sender.tab.url} with data: ${JSON.stringify(request.guesses)}`)
     if (request.guesses !== "hello"){
-      oneCall({guesses: request.guesses})
+      oneCall({guesses: request.guesses}).then(resp => {
+        wordsLeftText.innerText = resp.targets_left_len
+      })
       sendResponse({farewell: "goodbye"});
     }
       
   }
 );
-
-// chrome.storage.onChanged.addListener(function (changes, namespace) {
-//   for (let [key, { oldGuessList, newGuessList }] of Object.entries(changes)) {
-//     console.log(
-//       `Storage key "${key}" in namespace "${namespace}" changed.`,
-//       `Old list was "${oldGuessList}", new list is "${newGuessList}".`
-//     );
-//     oneCall({guesses: newGuessList})
-//   }
-// });
-
 
 // The body of this function will be execuetd as a content script inside the
 // current page
@@ -67,10 +52,6 @@ function getGuesses() {
     console.log(`sent guesses: ${JSON.stringify(guesses)}`)
     console.log(`got response: ${response.farewell}`);
   });
-
-  // chrome.storage.sync.set({guesses: guesses }, function() {
-  //   console.log('Value is set to ' + guesses);
-  // });
 
   return guesses;
 }
