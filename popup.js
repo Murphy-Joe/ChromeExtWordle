3f43821387165bf5ee55148195352e495cd098d1
Joe Murphy <murphcle@gmail.com>
2022-04-25 16:49:56 -0400

    words left page created

--- popup.js
+++ popup.js
@@ -1,13 +1,31 @@
 let wordsLeft = document.getElementById("wordsLeft")
 let bestLetters = document.getElementById("bestLetters")
 let bestGuess = document.getElementById("bestGuess")
+let refresh = document.getElementById("refresh");
+let landingPage = document.getElementById("landingPage")
+let wordsLeftPage = document.getElementById("wordsLeftPage")
+let wordsContainer = document.getElementById("wordsContainer")
+let backButtons = document.querySelectorAll(".back")
 
-async function callApi(guessPayload, endpoint) {
+let lastGuessList = []
+
+function createWordsLeftPage(resp) {
+  resp.targets.forEach(word => {
+    let wordBtn = document.createElement("button");
+    wordBtn.className = "word-btn";
+    wordBtn.innerText = word;
+    wordsContainer.appendChild(wordBtn);
+  });
+}
+
+async function callApi(msg, endpoint) {
+  const guessPayload = { guesses: msg.guesses }
   const response = await fetch(`https://1vv6d7.deta.dev/${endpoint}`, {
     method: 'POST',
     headers: {
       "Content-Type": "application/json",
     },
+
     body: JSON.stringify(guessPayload)
   })
   const resp = await response.json();
@@ -15,14 +33,30 @@ async function callApi(guessPayload, endpoint) {
   return resp;
 }
 
-function callApis(){
+function callApis(msg) {
+  wordsLeft.innerText = "Loading..."
+  callApi(msg, "targetsleft")
+    .then(resp => {
+      loadingWordsLeft = false;
+      wordsLeft.innerText = `${resp.count} Words Left`
+      createWordsLeftPage(resp);
+    })
 
+  bestLetters.innerText = "Loading..."
+  callApi(msg, "bestletters")
+    .then(resp => {
+      let letters = Object.keys(resp)
+      bestLetters.innerText = `Best ${letters.length} Letters`
+    })
+
+  bestGuess.innerText = "Loading..."
+  callApi(msg, "onecall")
+    .then(resp => {
+      bestGuess.innerText = `Best Guess: ${resp[0][0].toUpperCase()}`
+    })
 }
 
-function recieveMsg(request, sender, endpointToCall) {
-  // console.log(`received from: ${sender.tab.url}: ${JSON.stringify(request.guesses)}`)
-  return callApi({ guesses: request.guesses }, endpointToCall)
-}
+
 
 function runContentScript() {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -34,29 +68,32 @@ function runContentScript() {
     });
   });
 }
-let refresh = document.getElementById("refresh");
-refresh.addEventListener("click", runContentScript)
 
 runContentScript()
+refresh.addEventListener("click", runContentScript)
 
-chrome.runtime.onMessage.addListener((request, sender) => {
-  wordsLeft.innerText = "Loading..."
-  recieveMsg(request, sender, "targetsleft")
-    .then(resp => { 
-      loadingWordsLeft = false;
-      wordsLeft.innerText = `${resp.count} Words Left` 
-    })  
-  bestLetters.innerText = "Loading..."
-  recieveMsg(request, sender, "bestletters")
-    .then(resp => { 
-      let letters = Object.keys(resp)
-      bestLetters.innerText = `Best ${letters.length} Letters` 
-    })
-  bestGuess.innerText = "Loading..."
-  recieveMsg(request, sender, "onecall")
-    .then(resp => { 
-      bestGuess.innerText = `${bestGuess.innerText}: ${resp[0][0]}` 
-    })
+wordsLeft.addEventListener("click", () => {
+  landingPage.classList.add("hidden")
+  wordsLeftPage.classList.remove("hidden")
+})
+
+backButtons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    btn.parentElement.classList.add("hidden")
+    landingPage.classList.remove("hidden")
+  })
+})
+
+
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  if (JSON.stringify(msg.guesses) !== JSON.stringify(lastGuessList)) {
+    callApis(msg)
+    lastGuessList = msg.guesses
+  }
+  else {
+    console.log('no changes');
+  }
+  sendResponse({ farewell: "goodbye" }) // here till bug fix in chrome 102
 });
 
 // The body of this function will be execuetd as a content script inside the
@@ -65,9 +102,9 @@ function getGuesses() {
   let storage = JSON.parse(localStorage.getItem("nyt-wordle-state"))
   const guesses = storage.boardState
 
-  chrome.runtime.sendMessage({ guesses: guesses }, () => {
+  chrome.runtime.sendMessage({ guesses: guesses }, (response) => {
     console.log(`sent guesses: ${JSON.stringify(guesses)}`)
-    console.log(`got response: ${response.farewell}`);
+    // response here till bug fixed in chrome 102
   });
 
   return guesses;
