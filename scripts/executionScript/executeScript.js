a0783a56ab982326bd398d7d1a897b80015c6300
Murphy-Joe <ibidnsell@yahoo.com>
2022-05-13 15:34:55 -0400

    bracket fix

--- scripts/executionScript/executeScript.js
+++ scripts/executionScript/executeScript.js
@@ -5,55 +5,54 @@ export const getAndSendGuesses = () => {
   chrome.runtime.sendMessage({ guesses: guesses }, (response) => {
     console.log(`sent guesses: ${JSON.stringify(guesses)}`)
     // response here till bug fixed in chrome 102
+  })
 
-    chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-      sendResponse({ farewell: "goodbye" }) // here till bug fix in chrome 102
-      console.log(`IM LISTENING! I was sent a message: ${JSON.stringify(msg)}`);
-      if (msg.selectedGuess) {
-        console.log('entered listening if');
-        fillInGuess(msg.selectedGuess)
-      }
-    });
-    function fillInGuess(guess) {
-      console.log(`how the hell did I get here?`);
-      let storage = JSON.parse(localStorage.getItem("nyt-wordle-state"))
-      const guesses = storage.boardState
+  chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+    sendResponse({ farewell: "goodbye" }) // here till bug fix in chrome 102
+    console.log(`IM LISTENING! I was sent a message: ${JSON.stringify(msg)}`);
+    if (msg.selectedGuess) {
+      console.log('entered listening if');
+      fillInGuess(msg.selectedGuess)
+    }
+  });
+  function fillInGuess(guess) {
+    console.log(`how the hell did I get here?`);
+    let storage = JSON.parse(localStorage.getItem("nyt-wordle-state"))
+    const guesses = storage.boardState
 
-      const rowNthChild = getRowNthChild(guesses)
-      const selectedRow = getNthChildRow(rowNthChild)
-      const gameTiles = getGameTiles(selectedRow)
-      fillInTiles(gameTiles, guess)
+    const rowNthChild = getRowNthChild(guesses)
+    const selectedRow = getNthChildRow(rowNthChild)
+    const gameTiles = getGameTiles(selectedRow)
+    fillInTiles(gameTiles, guess)
 
-      function getRowNthChild(guesses) {
-        let nth = 1
-        guesses.forEach((guess) => {
-          if (guess) nth += 1
-        })
-        return nth;
-      }
-
-      function getNthChildRow(nthChild) {
-        const selectedRow = document.querySelector("body > game-app").shadowRoot.querySelector(`#board > game-row:nth-child(${nthChild})`)
-        console.log(selectedRow);
-        return selectedRow;
-      }
-
-      function getGameTiles(selectedRow) {
-        const tiles = selectedRow.shadowRoot.querySelector(".row").children
-        console.log(tiles);
-        return tiles;
-      }
-
-      function fillInTiles(tiles, guess) {
-        let idx = 0
-        for (let tile of tiles) {
-          tile.setAttribute("letter", guess[idx])
-          idx += 1
-        }
-      }
+    function getRowNthChild(guesses) {
+      let nth = 1
+      guesses.forEach((guess) => {
+        if (guess) nth += 1
+      })
+      return nth;
     }
 
-  });
+    function getNthChildRow(nthChild) {
+      const selectedRow = document.querySelector("body > game-app").shadowRoot.querySelector(`#board > game-row:nth-child(${nthChild})`)
+      console.log(selectedRow);
+      return selectedRow;
+    }
+
+    function getGameTiles(selectedRow) {
+      const tiles = selectedRow.shadowRoot.querySelector(".row").children
+      console.log(tiles);
+      return tiles;
+    }
+
+    function fillInTiles(tiles, guess) {
+      let idx = 0
+      for (let tile of tiles) {
+        tile.setAttribute("letter", guess[idx])
+        idx += 1
+      }
+    }
+  }
 
   return guesses;
 }
