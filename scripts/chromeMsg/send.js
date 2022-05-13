3d92960f92176480dbb907d5462d2091e76cdfd0
Murphy-Joe <ibidnsell@yahoo.com>
2022-05-13 11:10:22 -0400

    clear commented out code from chromeMsg send

--- scripts/chromeMsg/send.js
+++ scripts/chromeMsg/send.js
@@ -1,5 +1,3 @@
-// import {getAndSendGuesses} from '../contentScripts/contentScript.js';
-
 export function sendMsgToContentScript(passedInFunc) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.scripting.executeScript({
@@ -9,17 +7,3 @@ export function sendMsgToContentScript(passedInFunc) {
         });
       });
 }
-
-// export function onStartupRunContentScriptToGetAndSendGuesses() {
-//     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//       chrome.scripting.executeScript({
-//         target: { tabId: tabs[0].id },
-//         // reminder: the following function may not call other functions
-//         function: getAndSendGuesses,
-//       });
-//     });
-//   }
-
-// export function fillInSelectedGuess(guess) {
-
-// }
