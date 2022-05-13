c779404c46729152c5667348bc9906b48a38acbd
Murphy-Joe <ibidnsell@yahoo.com>
2022-05-13 15:01:11 -0400

    this still works

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
 import {addElementListeners} from './eventListeners/elementListeners.js';
-import {receiveGuesses} from './chromeMsg/receive.js';
+import {receiveGuesses, receiveGuessSelection} from './chromeMsg/receive.js';
 import {sendMsgToContentScriptGetAndSendGuesses} from './chromeMsg/send.js';
 
 sendMsgToContentScriptGetAndSendGuesses()
 addElementListeners();
 receiveGuesses()
+receiveGuessSelection()
