81e2c11a27d8161f798a3928f2c36642d09692cc
Murphy-Joe <ibidnsell@yahoo.com>
2022-05-13 11:42:07 -0400

    untested content script to fill in guess

--- scripts/contentScripts/contentScript.js
+++ scripts/contentScripts/contentScript.js
@@ -9,3 +9,39 @@ export const getAndSendGuesses = () => {
 
   return guesses;
 }
+
+export const fillInGuess = (guess) => {
+  let storage = JSON.parse(localStorage.getItem("nyt-wordle-state"))
+  const guesses = storage.boardState
+
+  const rowNthChild = getRowNthChild(guesses)
+  const selectedRow = getNthChildRow(rowNthChild)
+  const gameTiles = getGameTiles(selectedRow)
+  fillInTiles(gameTiles, "hello")
+
+  function getRowNthChild(guesses) {
+    let nth = 1
+    guesses.forEach((guess) => {
+      if (guess) nth += 1
+    })
+  }
+  
+  function getNthChildRow(nthChild) {
+    const selectedRow = document.querySelector("body > game-app").shadowRoot.querySelector(`#board > game-row:nth-child(${nthChild})`)
+    console.log(`selected row: ${selectedRow}`);
+    return selectedRow;
+  }
+
+  function getGameTiles(selectedRow) {
+    const tiles = selectedRow.shadowRoot.querySelector(".row").children
+    console.log(`tiles: ${tiles}`);
+    return tiles;
+  }
+
+  function fillInTiles(tiles, guess) {
+    tiles.forEach((tile, idx) => {
+      tile.setAttribute("letter", guess[idx])
+    })
+  }
+
+}
