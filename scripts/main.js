9ed7e73812f9a1b34728754b5a01ba5b82d5aa1e
Murphy-Joe <ibidnsell@yahoo.com>
2022-05-13 10:48:25 -0400

    forgot one

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,6 @@ import {addElementListeners} from './eventListeners/elementListeners.js';
 import {onStartupRunContentScriptToGetAndSendGuesses} from './chromeMsg/send.js';
 import {receiveGuesses} from './chromeMsg/receive.js';
 
-let lastGuessList = []
-
 onStartupRunContentScriptToGetAndSendGuesses()
 addElementListeners();
 receiveGuesses()
