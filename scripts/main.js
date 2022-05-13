ae02b8793b1f1197740b17c5e55f9abad845cf9b
Joe Murphy <murphcle@gmail.com>
2022-05-13 09:14:14 -0400

    moved content script and hardCoded

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,8 @@
 import {addEventListeners} from './eventListeners/listeners.js';
 import {callApis} from './api/apiCalls.js';
+import {getGuesses} from './content/contentScript.js';
 
 let lastGuessList = []
-
-function getGuesses() {
-  let storage = JSON.parse(localStorage.getItem("nyt-wordle-state"))
-  const guesses = storage.boardState
-
-  chrome.runtime.sendMessage({ guesses: guesses }, (response) => {
-    console.log(`sent guesses: ${JSON.stringify(guesses)}`)
-    // response here till bug fixed in chrome 102
-  });
-
-  return guesses;
-}
 
 function runContentScript() {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
